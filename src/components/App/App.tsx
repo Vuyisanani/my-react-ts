66578cbaad8926add79aa02daf86a0129024ec33
Vuyisanani Meteni <vuyisanani.meteni@SAMAC133.local>
2018-02-28 14:23:36 +0200

    Pictures

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -28,13 +28,21 @@ export class App extends React.Component<Props, State> {
 					 <code> The Horse Race Challenge </code> 
 				</p>
 				<div className="App-lane">
+					<img src="https://avatars2.githubusercontent.com/u/1482046?v=4" height="50"/><br/>
+
                      <Progress className="App-progress"/>
                  </div>
 				 <div className="App-lane">
                      <Progress className="App-progress"/>
+					 <img src="https://avatars0.githubusercontent.com/u/1819122?v=4" height="50"/><br/>
                  </div>
 				 <div className="App-lane">
                      <Progress className="App-progress"/>
+					 <img src="https://avatars2.githubusercontent.com/u/2323810?v=4" height="50"/><br/>
+                 </div>
+				 <div className="App-lane">
+                     <Progress className="App-progress"/>
+					 <img src="https://avatars3.githubusercontent.com/u/2363662?v=4" height="50"/><br/>
                  </div>
 
 			</div>
