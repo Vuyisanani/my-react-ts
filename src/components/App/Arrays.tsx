1a9f36fa5457cceab7ea0024c7c4bf77e6872141
Vuyisanani Meteni <vuyisanani.meteni@SAMAC133.local>
2018-02-28 13:16:34 +0200

    Recent

--- src/components/App/Arrays.tsx
+++ src/components/App/Arrays.tsx
@@ -1,34 +1,38 @@
-// import React, { Component } from 'react';
 import * as React from 'react';
 import Progress from 'react-progressbar';
 
 interface Props {
-    avatar: string,
-    color:string,
-    progress:number
+   avatar: string;
+   color:string;
+   progress:number;
 }
+
 interface State {
-    interval: number;
-    progress: number;
+   interval: number;
+   progress: number;
 }
 export class Arrays extends React.Component<Props, State> {
-    constructor(props: Props){
-        super(props);
-        this.state = {
-        interval: Math.floor(Math.random() * 200),
-        progress:  0,
-        }
-        this.timer = this.timer.bind(this);
-    }
+	constructor(props: Props){
+    	super(props);
+    	this.state = {
+      	interval: Math.floor(Math.random() * 200),
+    	progress:  0,
+    	}
+      this.timer = this.timer.bind(this);
+   }
+    
     componentDidMount() { 
         setInterval(this.timer, this.state.interval);
-      }
+
+    }
+
     timer = () => {
         this.setState({ progress: this.state.progress + 1 });
-        
         (this.state.progress >= 99) ? this.setState({ progress: 100 }) : "" ;
     }
+    
     break;
+    
     render() {
         return (
             <div>
